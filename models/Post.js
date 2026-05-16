8a2393dbb8547249c071c99087736787b29b98ca
ian <hope030327@naver.com>
2026-05-16 22:48:24 +0900

    fix: comment랑 post 모델 timestamps 옵션 true로 변경

--- models/Post.js
+++ models/Post.js
@@ -1,45 +1,49 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
-    post_id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+  const Post = sequelize.define(
+    "Post",
+    {
+      post_id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      board_id: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.BIGINT,
+        allowNull: false, // FK 없음, 값만 저장
+      },
+      author_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false, // 작성 시 user-svc에서 조회해서 저장
+      },
+      title: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
     },
-    board_id: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
+    {
+      tableName: "Post",
+      timestamps: true,
     },
-    user_id: {
-      type: DataTypes.BIGINT,
-      allowNull: false, // FK 없음, 값만 저장
-    },
-    author_name: {
-      type: DataTypes.STRING(50),
-      allowNull: false, // 작성 시 user-svc에서 조회해서 저장
-    },
-    title: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-  }, {
-    tableName: 'Post',
-    timestamps: false,
-  });
+  );
   return Post;
 };
